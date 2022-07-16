dac57a4dc354166dda0462ae474925b85a843687
Zakhar <ukhanovza@gmail.com>
2022-07-16 03:58:47 +0300

    \

--- src/window/Components/TodoList/TodoList.tsx
+++ src/window/Components/TodoList/TodoList.tsx
@@ -2,14 +2,14 @@ import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import styles from './TodoList.module.scss'
 //какая-то фигня с массивом, нужно разобраться с выводом его, не работает map
+//что-то с типами, послезавтара доделаю, скорее всего, нужно отдельным обьектом его делать
 const Todos: any = () =>{
     const todosList:any = useSelector(state => state.todos)
-  console.log(typeof todosList)
     return(
         <div className={styles.Todos__In}>
             <div className={styles.todo}>
               <div className={styles.todo__list}>
-                {todosList.map(e => console.log(e))}
+                {/* {todosList.map(e => console.log(e))} */}
                 <p>конец</p>
               </div>
             </div>
